5001d90b594ae9c4f7cfd59060b5235b76aef63d
v-yelina <heytridvaodin@gmail.com>
2021-10-04 21:44:47 +0200

    create component DeleteButton

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import EmployeeItem from "./EmployeeItem";
+import DeleteButton from "./DeleteButton";
 
 const EmployeeList = (props) => {
   return props.employees.map((employee) => (
@@ -17,27 +18,11 @@ const EmployeeList = (props) => {
         </div>
         <div className="d-flex align-items-center">
           <span>{employee.salary}</span>
-          <button
-            onClick={() => {
-              props.onDeleteHandler(employee.id);
-            }}
-            className="btn btn-light btn-sm rounded-1 px-2 ms-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-trash"
-              viewBox="0 0 16 16"
-            >
-              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-              <path
-                fill-rule="evenodd"
-                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-              />
-            </svg>
-          </button>
+          <DeleteButton
+            employees={props.employees}
+            employee={employee}
+            onDeleteHandler={props.onDeleteHandler}
+          />
         </div>
       </div>
     </EmployeeItem>
